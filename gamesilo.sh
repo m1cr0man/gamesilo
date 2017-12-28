46af78eda6e83f2ff9b362ca520825314e84c704
Lucas Savva <lucas@vccomputers.ie>
2017-12-28 21:46:40 +0000

    Add some global variables to the root script

--- gamesilo.sh
+++ gamesilo.sh
@@ -1,18 +1,19 @@
 #!/usr/bin/env bash
 
-DIRNAME=$(dirname $(readlink -f $0))
+export GS=$(readlink -f "$0")
+export GSDIR=$(dirname "$GS")
 
 function check_args() {
 	if [ $# -eq 1 ]; then
 		echo "Usage: $(basename $0) $1 action [args]"
 		echo -e "action\tOne of the following:"
-		ls -1 -d $DIRNAME/$1/* | xargs -L1 basename | cut -d'.' -f1 | xargs -L1 echo -e '\t'
+		basename -a "$GSDIR/$1"/[^_]* | cut -d'.' -f1 | xargs -L1 echo -e '\t'
 		echo -e "args\tleave action blank to get a list"
 		exit 1
 	elif [ $# -lt 2 ]; then
 		echo "Usage: $(basename $0) command action [args]"
 		echo -e "command\tOne of the following:"
-		ls -1 -d $DIRNAME/*/ | xargs -L1 basename | xargs -L1 echo -e '\t'
+		basename -a "$GSDIR"/[^_]*/ | xargs -L1 echo -e '\t'
 		echo -e "action\tLeave command blank to get a list"
 		echo -e "args\tLeave action blank to get a list"
 		exit 1
@@ -21,7 +22,7 @@ function check_args() {
 }
 
 function main() {
-	$DIRNAME/$1/$2.* ${@:3}
+	$GSDIR/$1/$2.* ${@:3}
 }
 
 check_args $*
