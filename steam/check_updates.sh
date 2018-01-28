2ceac9a6390e6ce4ef1659a3508324ae2c8d5c35
Lucas Savva <lucas@vccomputers.ie>
2018-01-28 17:59:04 +0000

    Add steam update checker

---
+++ steam/check_updates.sh
@@ -0,0 +1,80 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+. "$GSDIR/steam/_common.sh"
+
+function check_args() {
+	if [ $# -ne 1 ]; then
+		echo "Usage: $(basename "$0") library"
+		echo -e "library\tLibrary name"
+		exit 1
+	fi
+	check_library "$1"
+	return 0
+}
+
+function check_info() {
+	local fname="$1"
+	local appid="$(grep -m1 -Eo '[0-9]+' "$fname")"
+	local manifest="$steamapps/appmanifest_$appid.acf"
+
+	# Find "branches" section, set b=1
+	# Find "public" branch when b=1, set p=1
+	# Find "buildid" value when p=1, extract + print builid
+	local new_buildid="$(awk -F'"' '/branches/ { b=1 } b==1 && /public/ { p=1 } p==1 && /buildid/ { print $4; exit }' $fname)"
+
+	# Find "buildid", extract + print builid
+	local current_buildid="$(awk -F'"' '/buildid/ { print $4; exit }' "$manifest")"
+
+	if [ $new_buildid -gt $current_buildid ]; then
+		local name="$(awk -F'"' '/name/ { print $4; exit }' "$manifest")"
+		echo "$appid $name needs updating"
+	fi
+}
+
+function main() {
+	local library="$1"
+	local steamapps="$("$GS" steam _get_steamapps "$library")"
+
+	# Get a list of installed games
+	local appids="$(ls -1 "$steamapps"/appmanifest_*.acf | grep -Eo '[0-9]+' | sort -n)"
+
+	# Map each appid to an owner
+	local tmpdir="$(mktemp -d)"
+	echo "Mapping games to owners"
+	for appid in $appids; do
+		local owner="$("$GS" steam get_owners "$appid" | cut -d' ' -f1)"
+		echo "$appid" >> "$tmpdir/$owner"
+	done
+
+	# Download info for all games
+	for owner in $(ls -1 "$tmpdir"); do
+		echo "Getting info for games owned by $owner (total: $(wc -l "$tmpdir/$owner"))"
+		if "$GS" steam _run_container "$library" "$owner" $(awk '{ print "+app_info_print", $1 }' "$tmpdir/$owner") | tee "$tmpdir/$owner.info"; then
+			echo "Download successful"
+
+			# Turn the raw steamcmd output into separate files
+			local datapath="$tmpdir/${owner}_data"
+			mkdir -p "$datapath"
+
+			# Find "AppID :", increment, i, set x=1, go to next line
+			# When x == 1, write line to "temp$i"
+			# "x" stops extraneous output from steamcmd being written to a file
+			awk '/AppID \: .*/ { i++;flag=1;next } flag==1 { print > "'"$datapath"/'temp" i }' "$tmpdir/$owner.info"
+
+			# Read each file and print if it needs updating
+			for datafile in "$datapath"/*; do
+				check_info "$datafile"
+			done
+		else
+			>&2 echo "Failed to get info for $owner's games"
+		fi
+	done
+
+	rm -rf "$tmpdir"
+	echo "Done!"
+	return 0
+}
+
+check_args $*
+main $*
